bbd4836dfc7d3d6d8030fb09c12b211bc2d53107
Harsh Upadhyay <theharshup@gmail.com>
2026-06-25 19:07:44 +0530

    Add groq + secrets setup

--- utils/llm.py
+++ utils/llm.py
@@ -1,10 +1,9 @@
 from groq import Groq
 from dotenv import load_dotenv
-import os
-
+import streamlit as st
 load_dotenv()
 
-client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+client = Groq(api_key=st.secrets["GROQ_API_KEY"])
 
 
 def ask_llm(question, context_chunks):
